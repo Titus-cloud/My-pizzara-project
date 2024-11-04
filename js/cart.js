555ae85ab9f8c9fcb0559c906dbb30573dc87195
Titus Onzere <titusonzere2@gmail.com>
2024-11-04 17:44:50 +0300

    finished adding the adding item on the cart part left with price and increament part

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,24 @@
 const addToCartButtons = document.querySelectorAll(".v-c-bold");
 const counter = document.querySelector("span");
+const cartContainer = document.querySelector(".cart_container");
+const cartHeader = document.querySelector(".cart_hearder");
+const cartItemSection = document.querySelector(".cart_items_section");
+const cartIcon = document.querySelector(".cart");
+// const cartItemDetails = document.querySelector(".cartAll")
 // console.log(addToCartButtons)
 let allCartItems = [];
+
+cartIcon.addEventListener("click", (e) => {
+  // console.log(cartContainer)
+  cartContainer.classList.toggle("show-cart-container");
+  if (allCartItems.length === 0) {
+    cartHeader.style.display = "none";
+    cartItemSection.innerHTML = `<h3>No Item Added To Cart Yet</h3>`;
+  } else {
+    cartHeader.style.display = "flex";
+    updateCartItems()
+  }
+});
 
 //TODO: Here we make the add to cart buttons such that when you add to cart; the background changes to red and the txt changes to "Remove From Cart"
 // FIXME: First I looped througn the arr to find all the buttons using forEach
@@ -26,20 +43,48 @@ addToCartButtons.forEach((btn) => {
       btn.textContent = "Remove From Cart";
       btn.style.backgroundColor = "red";
 
+      // updating the cart when adding item
       allCartItems.push({ itemName: name, itemPrice: price, itemImage: image });
       console.log(allCartItems);
-      counter.textContent=allCartItems.length
+      counter.textContent = allCartItems.length;
       // Here incase you tap again it changes back to black and the txt changes to "Add to Cart"
     } else {
       btn.textContent = "Add to Cart";
       btn.style.backgroundColor = "black";
-    
-    const indexToRemove = allCartItems.findIndex((item) => item.itemName===name)
+
+      // Apdating the cart when removing item
+      const indexToRemove = allCartItems.findIndex(
+        (item) => item.itemName === name
+      );
       if (indexToRemove !== -1) {
         allCartItems.splice(indexToRemove, 1);
-        counter.textContent=allCartItems.length
+        counter.textContent = allCartItems.length;
       }
     }
-    });
   });
-;
+});
+
+function updateCartItems(){
+  // clear first
+  cartItemSection.innerHTML = ""
+  allCartItems.map((cartItem) => {
+    let cartAll=document.createElement("div")
+    cartAll.classList.add('cart_items')
+    cartAll.innerHTML =`
+     <div class="cartImg">
+            <img
+              src="images/tandoori-chicken.png"
+              alt="Tandoori Chicken Pizza"
+            />
+          </div>
+          <p style="flex:1">${cartItem.itemName}<br /></p>
+          <p style="flex:1"">Ksh. 1500</p>
+          <div class="quants" style="flex:1">
+            <p>-</p>
+            <p>1</p>
+            <p>+</p>
+          </div>
+    `
+    cartItemSection.append(cartAll)
+  })
+}
